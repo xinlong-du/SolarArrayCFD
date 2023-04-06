05d0bdeb8b4d5205a7284569c0b0c1f5de23ed0d
Xinlong Du <du.xinl@husky.neu.edu>
2023-04-06 14:02:01 -0700

    Add probes for pressure taps

--- windTunnel/pressureTaps.m
+++ windTunnel/pressureTaps.m
@@ -1,18 +1,18 @@
 close all; clear; clc;
-L=4.29895;
+L=4.29895/36;
 tilt=30/180*pi;
-xRefTop=69.90789;
-yRefTop=1.517623;
-zRefTop=64.540925;
-xRefBot=69.92289;
-yRefBot=1.491642;
-zRefBot=64.540925;
+xRefTop=1.941897;
+yRefTop=3.142196;
+zRefTop=0.042157;
+xRefBot=1.942314;
+yRefBot=3.142196;
+zRefBot=0.041435;
 xyzTapTop=tapCoord(L,tilt,xRefTop,yRefTop,zRefTop);
 xyzTapBot=tapCoord(L,tilt,xRefBot,yRefBot,zRefBot);
 %% function for calculating tap locations
 function xyzTap=tapCoord(L,tilt,xRef,yRef,zRef)
 dX=L*repmat([0.125;0.125+0.25;0.125+0.25+0.25;0.125+0.25+0.25+0.25],7,1);
-dz=L*[repmat(0.125,4,1);...
+dy=-L*[repmat(0.125,4,1);...
     repmat(0.125+0.5,4,1);...
     repmat(0.125+0.5+0.75,4,1);...
     repmat(0.125+0.5+0.75+1,4,1);...
@@ -20,11 +20,14 @@ dz=L*[repmat(0.125,4,1);...
     repmat(0.125+0.5+0.75+1+1.25+1,4,1);...
     repmat(0.125+0.5+0.75+1+1.25+1+0.75,4,1)];
 dx=dX*cos(tilt);
-dy=dX*sin(tilt);
+dz=dX*sin(tilt);
 xTap=xRef+dx;
 yTap=yRef+dy;
 zTap=zRef+dz;
 xyzTap=[xTap,yTap,zTap];
 figure
-scatter3(xTap,zTap,yTap)
+scatter3(xTap,yTap,zTap)
+xlabel('X')
+ylabel('Y')
+zlabel('Z')
 end
