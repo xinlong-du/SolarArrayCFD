34a9e1d99d4a0f6be3ec5f80202576b499602f22
Xinlong Du <du.xinl@husky.neu.edu>
2023-04-14 00:37:27 -0700

    Update inflow properties with new RWDI data

--- windTunnel/inflowProperties.m
+++ windTunnel/inflowProperties.m
@@ -1,7 +1,8 @@
 close all; clear; clc;
-L=4.29895/36;    %chord length
-Zref=0.6*L;   %referenceDist
-Uref=3;       %referenceValue 18/6=3m/s
+L=4.29895/30;    %chord length
+ZrefdL=0.7;
+Zref=ZrefdL*L;   %referenceDist
+Uref=9;          %referenceValue (m/s)
 windProfile=readtable('../../../RWDI/Inflow/windProfile.txt');
 ZdL=windProfile.z_L_c;
 UdUref=windProfile.U_U_ref;
@@ -18,11 +19,11 @@ alpha=0.15;   %power law
 figure
 plot(UdUref,ZdL,'o','LineWidth',1)
 hold on
-x=0.5:0.01:1.4;                                 %U/Uref
-plot(x,exp(log(x)/alpha)*0.6,'k','LineWidth',1) %0.6 is used to convert Z/Zref to Z/L
-y=0:0.02:4;                                     %Z/L
-plot((y/0.6).^alpha,y,'r--','LineWidth',1)      %0.6 is used to convert Z/L to Z/Zref
-legend('Measured','(Z/Zref)^{0.15}')
+x=0.5:0.01:1.4;                                      %U/Uref
+plot(x,exp(log(x)/alpha)*ZrefdL,'k','LineWidth',1)   %ZrefdL is used to convert Z/Zref to Z/L
+y=0:0.02:4;                                          %Z/L
+plot((y/ZrefdL).^alpha,y,'r--','LineWidth',1)        %ZrefdL is used to convert Z/L to Z/Zref
+legend('Measured','(Z/Zref)^{0.15}','(Z/Zref)^{0.15}')
 legend('Location','Northwest')
 xlabel('U/Uref')
 ylabel('z/L')
@@ -30,28 +31,26 @@ xlim([0.5,1.4])
 ylim([0.0,4.0])
 
 %linear regression
-ZdZref=ZdL/0.6;
+ZdZref=ZdL/ZrefdL;
 X=log(ZdZref);
 Y=log(UdUref);
 alphaU=(X'*X)\(X'*Y);
 figure
 plot(UdUref,ZdL,'o','LineWidth',1)
 hold on
-x=0.5:0.01:1.4;                                  %U/Uref
-plot(x,exp(log(x)/alphaU)*0.6,'k','LineWidth',1) %0.6 is used to convert Z/Zref to Z/L
-y=0:0.02:4;                                      %Z/L
-plot((y/0.6).^alphaU,y,'r--','LineWidth',1)      %0.6 is used to convert Z/L to Z/Zref
-legend('Measured','(Z/Zref)^{0.1187}')
+x=0.5:0.01:1.4;                                       %U/Uref
+plot(x,exp(log(x)/alphaU)*ZrefdL,'k','LineWidth',1)   %ZrefdL is used to convert Z/Zref to Z/L
+y=0:0.02:4;                                           %Z/L
+plot((y/ZrefdL).^alphaU,y,'r--','LineWidth',1)        %ZrefdL is used to convert Z/L to Z/Zref
+legend('Measured','(Z/Zref)^{0.1349}','(Z/Zref)^{0.1349}')
 legend('Location','Northwest')
 xlabel('U/Uref')
 ylabel('z/L')
 xlim([0.5,1.4])
 ylim([0.0,4.0])
 %% RDict
-ZrefI=0.62*L;
-ZdZrefI=ZdL/0.62;
-X=log(ZdZrefI);
-IudIuref=Iu/Iu(1);
+Iuref=(Iu(2)-Iu(1))*(ZrefdL-ZdL(1))/(ZdL(2)-ZdL(1))+Iu(1);
+IudIuref=Iu/Iuref;
 Y=log(IudIuref);
 alphaI=(X'*X)\(X'*Y);
 
@@ -66,17 +65,17 @@ plot(Iu,ZdL,'bo','LineWidth',1)
 hold on
 plot(Iv,ZdL,'rsquare','LineWidth',1)
 plot(Iw,ZdL,'gv','LineWidth',1)
-plot(Iu(1)*(y/0.62).^alphaI,y,'k','LineWidth',1)
-plot(Ivref*(y/0.62).^alphaI,y,'r','LineWidth',1)
-plot(Iwref*(y/0.62).^alphaI,y,'g','LineWidth',1)
-legend('Measured Iu','Measured Iv','Measured Iw','Iuref*(Z/Zref)^{-0.2243}','Ivref*(Z/Zref)^{-0.2243}','Iwref*(Z/Zref)^{-0.2243}')
+plot(Iuref*(y/ZrefdL).^alphaI,y,'k','LineWidth',1)
+plot(Ivref*(y/ZrefdL).^alphaI,y,'r','LineWidth',1)
+plot(Iwref*(y/ZrefdL).^alphaI,y,'g','LineWidth',1)
+legend('Measured Iu','Measured Iv','Measured Iw','Iuref*(Z/Zref)^{-0.2254}','Ivref*(Z/Zref)^{-0.2254}','Iwref*(Z/Zref)^{-0.2254}')
 legend('Location','Northwest')
 xlabel('Turbulence Intensity')
 ylabel('z/L')
 xlim([0.0,0.25])
 ylim([0.0,4.0])
 
-RuuRef=(Iu(1)*Uref)^2;
+RuuRef=(Iuref*Uref)^2;
 RvvRef=(Ivref*Uref)^2;
 RwwRef=(Iwref*Uref)^2;
 alphaRuu=2*(alphaI+alphaU);
