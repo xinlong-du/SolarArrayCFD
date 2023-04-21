c9848964a70fd8037bfa1d00438d88a0dc847b1f
Xinlong Du <du.xinl@husky.neu.edu>
2023-04-20 22:50:59 -0700

    Slightly reduce height of bottom pressure taps and organize files
    to avoid locating bottom pressure taps within the panel. If not, will output incorrect pressure.

--- windTunnel/pressureTaps.m
+++ windTunnel/pressureTaps.m
@@ -6,9 +6,16 @@ yRefTop=3.142196;
 zRefTop=0.042157;
 xRefBot=1.942314;
 yRefBot=3.142196;
-zRefBot=0.041435;
+zRefBot=0.041434;
 xyzTapTop=tapCoord(L,tilt,xRefTop,yRefTop,zRefTop);
 xyzTapBot=tapCoord(L,tilt,xRefBot,yRefBot,zRefBot);
+figure
+scatter3(xyzTapTop(:,1),xyzTapTop(:,2),xyzTapTop(:,3),'k.')
+hold on
+scatter3(xyzTapBot(:,1),xyzTapBot(:,2),xyzTapBot(:,3),'r.')
+xlabel('X')
+ylabel('Y')
+zlabel('Z')
 %% function for calculating tap locations
 function xyzTap=tapCoord(L,tilt,xRef,yRef,zRef)
 dX=L*repmat([0.125;0.125+0.25;0.125+0.25+0.25;0.125+0.25+0.25+0.25],7,1);
