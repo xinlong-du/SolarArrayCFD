d8bbc073e740598b51e9ddad025d11a508845b0b
Xinlong Du <du.xinl@husky.neu.edu>
2023-04-21 18:27:37 -0700

    Compare Cp on each pressure tap

--- windTunnel/pressureCoeff.m
+++ windTunnel/pressureCoeff.m
@@ -4,117 +4,72 @@ filename = '../../../RWDI/Wind Tunnel Data/tilt_n30deg.hdf5';
 % h5disp(filename);
 info = h5info(filename);
 level2 = info.Groups(1);
-data = h5read(filename,'/WindDir_0deg/Row7');
+CpRWDI = h5read(filename,'/WindDir_0deg/Row7');
 dtNorm = h5read(filename,'/WindDir_0deg/dtNorm');
 
-%% frequency analysis
-L=4.29895/30;
-U=9;
-dt=dtNorm*L/U;
-Fs=1/dt;
-Cp=data(1,:);
-nfftS=1024*16; % number of Fourier Points (resolution)
-% Suu: ONE-SIDED PSD
-% n: frequency, Hz
-[Su1,n1]=periodogram(Cp,[],'onesided',nfftS,Fs);
+%% CFD data
+L=4.29895/36; %chord length
+U=3;          %wind speed
+dtCFD=0.005;  %output time step
 
-NBlock = 3; % number of blocks
-N=length(Cp);
-Ncoh = round(N/NBlock); % number of data point per block
-[Su2,n2]=pwelch(Cp,Ncoh,round(Ncoh/2),Ncoh,Fs);
+p = readtable('./Data/pCopy');
+timeCFD=p.Var1;
+pTop=p{:,2:29};
+pBot=p{:,170:197};
+pNet=pTop-pBot;
+CpCFD=pNet/(0.5*U^2); %The pressure is kinematic pressure pk=ps/rho (m^2/s^2)
+
+%% compare each pressure tap
+dtRWDI=dtNorm*L/U;
+for tapID=1:28
+    CpRWDItap=CpRWDI(tapID,:);
+    CpCFDtap=CpCFD(:,tapID);
+    comparePSD(dtRWDI,dtCFD,timeCFD,CpRWDItap,CpCFDtap,tapID)
+end
+
+%% function for frequency analysis
+function comparePSD(dtRWDI,dtCFD,timeCFD,CpRWDItap,CpCFDtap,tapID)
+nfftS=1024*16; % number of Fourier Points (resolution)
+%RWDI
+Fs=1/dtRWDI;
+[SuuRWDI,nRWDI]=periodogram(CpRWDItap,[],'onesided',nfftS,Fs); % Suu: ONE-SIDED PSD, n: frequency, Hz
+
+%CFD
+Fs=1/dtCFD;
+[SuuCFD,nCFD]=periodogram(CpCFDtap,[],'onesided',nfftS,Fs);
 
 % Plotting PSD data on log-log axes
-figure
-loglog(n1,Su1)
+hfig=figure;
+loglog(nRWDI,SuuRWDI)
 hold on
-loglog(n2,Su2)
-legend('periodogram','pwelch')
-xlabel('Frequency')
-ylabel('Power Spectral Density')
+loglog(nCFD,SuuCFD)
+legend({'RWDI','CFD'},'FontSize',8,'FontName','Times New Roman')
+xlabel('Frequency','FontSize',8,'FontName','Times New Roman')
+ylabel('Power Spectral Density','FontSize',8,'FontName','Times New Roman')
+set(gca,'FontSize',8,'FontName','Times New Roman')
+% save figure
+figWidth=3.5;
+figHeight=3;
+set(hfig,'PaperUnits','inches');
+set(hfig,'PaperPosition',[0 0 figWidth figHeight]);
+fileout=strcat('.\Output\PSDtap',num2str(tapID),'.');
+print(hfig,[fileout,'tif'],'-r300','-dtiff');
 
-% fft
-Y=fft(Cp);
-n = length(Cp);          % number of samples
-f = (0:n-1)*(Fs/n);     % frequency range
-power = abs(Y).^2/n;    % power of the DFT
-figure
-loglog(f,power)
-xlabel('Frequency')
-ylabel('Power')
-%% CFD data
-tap1Top = readtable('tilt_n30Azim0Row7Tap1Top.csv');
-tap1Bot = readtable('tilt_n30Azim0Row7Tap1Bot.csv');
-tap2Top = readtable('tilt_n30Azim0Row7Tap2Top.csv');
-tap2Bot = readtable('tilt_n30Azim0Row7Tap2Bot.csv');
-tap3Top = readtable('tilt_n30Azim0Row7Tap3Top.csv');
-tap3Bot = readtable('tilt_n30Azim0Row7Tap3Bot.csv');
-tap4Top = readtable('tilt_n30Azim0Row7Tap4Top.csv');
-tap4Bot = readtable('tilt_n30Azim0Row7Tap4Bot.csv');
-tap5Top = readtable('tilt_n30Azim0Row7Tap5Top.csv');
-tap5Bot = readtable('tilt_n30Azim0Row7Tap5Bot.csv');
-tap6Top = readtable('tilt_n30Azim0Row7Tap6Top.csv');
-tap6Bot = readtable('tilt_n30Azim0Row7Tap6Bot.csv');
-tap7Top = readtable('tilt_n30Azim0Row7Tap7Top.csv');
-tap7Bot = readtable('tilt_n30Azim0Row7Tap7Bot.csv');
-tap8Top = readtable('tilt_n30Azim0Row7Tap8Top.csv');
-tap8Bot = readtable('tilt_n30Azim0Row7Tap8Bot.csv');
-
-[pCoeff(:,1),meanRWDI(1),meanCFD(1)]=calpCoeff(tap1Top,tap1Bot,data,dtNorm,1);
-[pCoeff(:,2),meanRWDI(2),meanCFD(2)]=calpCoeff(tap2Top,tap2Bot,data,dtNorm,2);
-[pCoeff(:,3),meanRWDI(3),meanCFD(3)]=calpCoeff(tap3Top,tap3Bot,data,dtNorm,3);
-[pCoeff(:,4),meanRWDI(4),meanCFD(4)]=calpCoeff(tap4Top,tap4Bot,data,dtNorm,4);
-[pCoeff(:,5),meanRWDI(5),meanCFD(5)]=calpCoeff(tap5Top,tap5Bot,data,dtNorm,5);
-[pCoeff(:,6),meanRWDI(6),meanCFD(6)]=calpCoeff(tap6Top,tap6Bot,data,dtNorm,6);
-[pCoeff(:,7),meanRWDI(7),meanCFD(7)]=calpCoeff(tap7Top,tap7Bot,data,dtNorm,7);
-[pCoeff(:,8),meanRWDI(8),meanCFD(8)]=calpCoeff(tap8Top,tap8Bot,data,dtNorm,8);
-%%
-tapIDs=1:8;
-figure
-plot(tapIDs,meanRWDI)
+% plot time series
+timeRWDI=dtRWDI:dtRWDI:dtRWDI*round(timeCFD(end)/dtRWDI);
+hfig=figure;
+plot(timeRWDI,CpRWDItap(1:round(timeCFD(end)/dtRWDI)))
 hold on
-plot (tapIDs,meanCFD)
-xlabel('Pressure tap ID')
-ylabel('Pressure coefficient')
-legend('RWDI','CFD')
-%% mean over space
-mean2RWDI=mean(meanRWDI);
-mean2CFD=mean(meanCFD);
-%%
-function [pCoeff,meanRWDI,meanCFD]=calpCoeff(top,bot,data,dtNorm,tapID)
-rho=1.0; %The pressure is kinematic pressure pk=ps/rho (m^2/s^2)
-pTop=[top.avg_p_];
-pBot=[bot.avg_p_];
-% U=top.avg_U_0__;
-U=18;
-pCoeff=(pTop-pBot)./(0.5*rho*U.^2);
-
-%% compare
-L=4.29895;
-dt=dtNorm*L/mean(U);
-timeCFD=top.Time;
-timeRWDI=dt:dt:round(timeCFD(end)/dt)*dt;
-figure
-plot(timeCFD,pCoeff)
-hold on
-plot(timeRWDI,data(1,1:round(timeCFD(end)/dt)))
-legend('CFD','RWDI')
-xlabel('Time (s)')
-ylabel('Pressure Coefficient')
-
-figure
-plot(timeCFD,pTop)
-hold on
-plot(timeCFD,pBot)
-legend('Top pressure','Bottom pressure')
-xlabel('Time (s)')
-ylabel('Pressure (m^2/s^2)')
-
-%% statistics
-meanRWDI=mean(data(tapID,:));
-meanCFD=mean(pCoeff);
-figure
-plot(dt:dt:length(data)*dt,data(tapID,:))
-legend('CFD','RWDI')
-xlabel('Time (s)')
-ylabel('Pressure Coefficient')
+plot(timeCFD,CpCFDtap)
+legend({'RWDI','CFD'},'FontSize',8,'FontName','Times New Roman')
+xlabel('Time (s)','FontSize',8,'FontName','Times New Roman')
+ylabel('Pressure Coefficient','FontSize',8,'FontName','Times New Roman')
+set(gca,'FontSize',8,'FontName','Times New Roman')
+% save figure
+figWidth=6;
+figHeight=3;
+set(hfig,'PaperUnits','inches');
+set(hfig,'PaperPosition',[0 0 figWidth figHeight]);
+fileout=strcat('.\Output\timeSeriesTap',num2str(tapID),'.');
+print(hfig,[fileout,'tif'],'-r300','-dtiff');
 end
