dc9bd71f7e3f4117bf16be179f96c039c8c7bd9a
Xinlong Du <du.xinl@husky.neu.edu>
2023-05-05 00:01:15 -0400

    Make sure probe locations be outside of panel
    To avoid incorrect large pressure outputs

--- windTunnel/pressureTaps.m
+++ windTunnel/pressureTaps.m
@@ -3,10 +3,10 @@ L=4.29895/30;
 tilt=30/180*pi;
 xRefTop=[0.613855;0.900455;1.187055;1.473655;1.760255;2.046855;2.333455];
 yRefTop=3.957359;
-zRefTop=0.051411;
+zRefTop=0.051412;
 xRefBot=[0.615305;0.901905;1.188505;1.475105;1.761705;2.048305;2.334905];
 yRefBot=3.957359;
-zRefBot=0.0489;
+zRefBot=0.048899;
 xyzTapTop=zeros(28,21);
 xyzTapBot=zeros(28,21);
 
@@ -25,6 +25,21 @@ xlabel('X')
 ylabel('Y')
 zlabel('Z')
 axis equal
+
+%% write probe locations
+fileID=fopen('probes.txt','w');
+for i = 0:6
+    fprintf(fileID,'%6s %1i %1s\n','// row',i+1,'pressure taps on the top of the panel');
+    for j=1:28
+        fprintf(fileID,'%1s%18.16f %18.16f %18.16f%1s\n','(',xyzTapTop(j,3*i+1:3*i+3),')');
+    end
+    
+    fprintf(fileID,'%6s %1i %1s\n','// row',i+1,'pressure taps on the bottom of the panel');
+    for j=1:28
+        fprintf(fileID,'%1s%18.16f %18.16f %18.16f%1s\n','(',xyzTapBot(j,3*i+1:3*i+3),')');
+    end
+end
+fclose(fileID);
 %% function for calculating tap locations
 function xyzTap=tapCoord(L,tilt,xRef,yRef,zRef)
 dX=L*repmat([0.125;0.125+0.25;0.125+0.25+0.25;0.125+0.25+0.25+0.25],7,1);
