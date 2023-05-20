7cc32d8eb3f695ff4f9e579d156cd71d51ad59f7
Xinlong Du <du.xinl@husky.neu.edu>
2023-05-19 23:39:36 -0700

    Compare inlet and incident wind profiles

--- windTunnel/verifyInflow.m
+++ windTunnel/verifyInflow.m
@@ -18,7 +18,7 @@ U_RWDI=UdUref*Uref;
 
 %%
 timeCFD=U.Var1;
-Uinlet=U{:,2:11};
+Uinlet=U{:,2:11}; %probe locations: +0(x=0.0,y=3.1), +10(x=0.1,y=3.1), +20(x=0.6,y=3.1), +30(x=0.0,y=2.7), +40(x=0.1,y=2.7), +50(x=0.6,y=2.7)
 idx=strfind(Uinlet,')');
 UxInlet=zeros(size(Uinlet));
 UyInlet=zeros(size(Uinlet));
@@ -39,17 +39,59 @@ stdUxInlet=std(UxInlet);
 stdUyInlet=std(UyInlet);
 stdUzInlet=std(UzInlet);
 IxCFD=stdUxInlet./meanUxInlet;
-IyCFD=stdUyInlet; %how to calculate turbulence intensity if mean is close to 0
-IzCFD=stdUzInlet;
+%For Ix and Iy, how to calculate turbulence intensity if mean is close to 0?
+
 %%
 figure
-plot(U_RWDI,Z_RWDI)
+plot(U_RWDI,Z_RWDI,'k','LineWidth',1)
 hold on
-plot(meanUxInlet,Z_CFD)
-legend('RWDI','CFD')
+plot(meanUxInlet,Z_CFD,'r--','LineWidth',1)
+legend({'RWDI mean','CFD mean'},'FontSize',8,'FontName','Times New Roman')
+legend('Location','Northwest')
+xlabel('U (m/s)','FontSize',8,'FontName','Times New Roman')
+ylabel('Z (m)','FontSize',8,'FontName','Times New Roman')
+set(gca,'FontSize',8,'FontName','Times New Roman')
 
 figure
-plot(IuRWDI,Z_RWDI)
+plot(IuRWDI,Z_RWDI,'k','LineWidth',1)
 hold on
-plot(IxCFD,Z_CFD)
-legend('RWDI','CFD')
+plot(IxCFD,Z_CFD,'r--','LineWidth',1)
+legend({'RWDI mean','CFD mean'},'FontSize',8,'FontName','Times New Roman')
+legend('Location','Northeast')
+xlabel('Iu','FontSize',8,'FontName','Times New Roman')
+ylabel('Z (m)','FontSize',8,'FontName','Times New Roman')
+set(gca,'FontSize',8,'FontName','Times New Roman')
+
+Iuref=(IuRWDI(2)-IuRWDI(1))*(ZrefdL-ZdL(1))/(ZdL(2)-ZdL(1))+IuRWDI(1);
+IudIuref=IuRWDI/Iuref;
+ZdZref=ZdL/ZrefdL;
+X=log(ZdZref);
+Y=log(UdUref);
+alphaU=(X'*X)\(X'*Y);
+Y=log(IudIuref);
+alphaI=(X'*X)\(X'*Y);
+y=0:0.02:7;
+
+figure
+plot(UdUref,ZdL,'bo','LineWidth',1)
+hold on
+plot((y/ZrefdL).^alphaU,y,'k','LineWidth',1)
+plot(meanUxInlet/Uref,Z_CFD/L,'r--','LineWidth',1)
+legend({'Measured','(Z/Zref)^{0.1349}','CFD'},'FontSize',8,'FontName','Times New Roman')
+legend('Location','Northwest')
+xlabel('U/Uref','FontSize',8,'FontName','Times New Roman')
+ylabel('Z/L','FontSize',8,'FontName','Times New Roman')
+set(gca,'FontSize',8,'FontName','Times New Roman')
+xlim([0.5,1.4])
+
+figure
+plot(IuRWDI,ZdL,'bo','LineWidth',1)
+hold on
+plot(Iuref*(y/ZrefdL).^alphaI,y,'k','LineWidth',1)
+plot(IxCFD,Z_CFD/L,'r--','LineWidth',1)
+legend({'Measured Iu','Iuref*(Z/Zref)^{-0.2254}','CFD'},'FontSize',8,'FontName','Times New Roman')
+legend('Location','Northeast')
+xlabel('Iu','FontSize',8,'FontName','Times New Roman')
+ylabel('Z/L','FontSize',8,'FontName','Times New Roman')
+set(gca,'FontSize',8,'FontName','Times New Roman')
+xlim([0.0,0.25])
