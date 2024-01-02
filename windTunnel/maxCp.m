06cd39051e2b8917a1f6c985738e4fe1f85d51a6
Xinlong Du <xinlongdu@berkeley.edu>
2024-01-01 19:03:59 -0800

    Find max of mean of Cp and the corresponding dt and duration

--- windTunnel/maxCp.m
+++ windTunnel/maxCp.m
@@ -1,10 +1,17 @@
 close all; clear; clc;
 
-maximumCp=zeros(8,1);
+maxiCp=zeros(8,1);
+maxiCpMean=zeros(8,3);
 % inputs
 tilt={'n30','p30'};
 dir={'0','30','60','90'};
 rowID=[1,7];
+
+%% full scale data
+in2m=0.0254;   %inch to meter
+L=169.25*in2m; %m
+U=27.0;        %m/s
+rho_air=1.226;
 
 %% wind tunnel data
 k=0;
@@ -18,7 +25,16 @@ for i=1:2
         CpRWDI = h5read(filename,strcat('/WindDir_',dir{j},'deg/Row',num2str(rowID(i))));
         CpRWDI = CpRWDI';
         dtNorm = h5read(filename,strcat('/WindDir_',dir{j},'deg/dtNorm'));
-
-        maximumCp(k)=max(max(abs(CpRWDI)));
+        
+        maxiCp(k)=max(max(abs(CpRWDI)));
+        CpMean=mean(CpRWDI,2);
+        maxiCpMean(k,1)=max(abs(CpMean));
+        
+        dt=dtNorm*L/U;
+        dura=dt*length(CpMean);
+        maxiCpMean(k,2)=dt;
+        maxiCpMean(k,3)=dura;
+        
+        p=0.5*rho_air*U*U*CpMean;
     end
 end
